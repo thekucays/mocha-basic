290cb0020b149357f7bd7816933b09544de25c11
Luki R <thekucays@gmail.com>
2025-04-10 22:38:50 +0700

    feat: add test hook and headless mode

---
+++ tests/sesi-10/test_sesi10.js
@@ -0,0 +1,44 @@
+const { Builder, By, until } = require('selenium-webdriver');
+const assert = require('assert');
+const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
+
+describe('Google Search Test', function () {
+    let driver;
+
+// before(async function () {
+//     driver = await new Builder().forBrowser('chrome').build();
+// });
+
+// after(async function () {
+//     await driver.quit();
+// });
+
+    it('Headless Chrome - Visit SauceDemo dan cek page title', async function () {
+        options = new chrome.Options();
+        options.addArguments("--headless");
+
+        driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+
+        // driver = await new Builder().forBrowser('chrome').build();
+
+        await driver.get('https://www.saucedemo.com');
+        const title = await driver.getTitle();
+
+        await driver.quit()
+    })
+
+    it('Headless Firefox - Visit SauceDemo dan cek page title', async function () {
+        options = new firefox.Options()
+        options.addArguments("--headless");
+
+        driver = await new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
+
+        // driver = await new Builder().forBrowser('chrome').build();
+
+        await driver.get('https://www.saucedemo.com');
+        const title = await driver.getTitle();
+
+        await driver.quit()
+    })
+});
